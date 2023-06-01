744cf392be48cff943a91fc9a6f38badb83dd232
Ubuntu <ubuntu@ip-172-31-10-164.us-east-2.compute.internal>
2023-06-01 01:54:13 +0000

    First commit

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,12 +1,5 @@
 import {io} from 'socket.io-client';
 
-const options = {
-  "force new connection": true,
-  reconnectionAttempts: "Infinity", // avoid having user reconnect manually in order to prevent dead clients after a server restart
-  timeout : 10000, // before connect_error and connect_timeout are emitted.
-  transports : ["websocket"]
-}
-
-const socket = io('http://localhost:3000/', options);
+const socket = io("https://idocdevopsprojects.com/",{ forceNew: true, reconnectionAttempts: "Infinity", timeout : 10000});
 
 export default socket;
